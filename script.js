92693a3c18aeee424649ac09bd1872634e851946
Archange Mbah Tchategouang <archange.mbah.tchategouang@mni.thm.de>
2024-05-06 23:36:50 +0200

    Button-reload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let currMoleTile=null;
 let currPlantTile=null;
 let score=0;
 let gameOver=false;
+let gameOverButton=document.createElement("button"); // Changed from div to button
 
 window.onload=function(){
     setGame();
@@ -14,8 +15,9 @@ function setGame(){
         document.getElementById("board").appendChild(tile); //tile becomes child of board
     }
     setInterval(setMole,1000);
-    setInterval(setPlant,2000);
-    }
+    setInterval(setPlant,1000);
+   
+}
 
     function getRandomTile(){ //returns a random number between 0 and 8 to set the mole in a random tile
         let num=Math.floor(Math.random()*9);
@@ -66,13 +68,39 @@ function setGame(){
             score+=10;
             document.getElementById("score").innerHTML=score.toString();
 
+            if(score>=100){
+                gameOver=true;
+                displayGameOver();
+            }
+
         }
 
         else if(this == currPlantTile){
             score-=5;
             document.getElementById("score").innerHTML=" Game OVer :" +score.toString();
             gameOver=true;
+            displayGameOver();
         }
-
-        
     }
+
+    function displayGameOver(){
+        if(score>=100){
+            gameOverButton.innerHTML="You Win! Your score is: "+score.toString();
+        
+        }
+        else {
+            gameOverButton.innerHTML="Game Over! Your score is: "+score.toString();
+    }
+        document.body.style.backgroundColor = "rgba(0, 0, 0, 0.5)"; // Dark background
+        document.getElementById("board").style.display = "none"; // Hide board
+        document.getElementById("score").style.display = "none"; // Hide score
+        document.body.appendChild(gameOverButton); // Append button to body
+        gameOverButton.onclick=function(){
+            resetGame();
+    }
+}
+function resetGame(){
+    window.location.reload(); // Reload the page
+}
+        
+    
